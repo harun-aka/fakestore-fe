169866e86f48b04397b33759460c84aad9aea6a0
Harun AKA <harrun01@hotmail.com>
2022-04-08 00:05:21 +0300

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// app.use(express.static(__dirname + '/dist/fakestore-fe'));
+// app.get('/*', function(req,res) {
+// res.sendFile(path.join(__dirname+
+// '/dist/fakestore-fe/index.html'));});
+// app.listen(process.env.PORT || 8080);
+
+function requireHTTPS(req, res, next) {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 const express = require('express');
-const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/fakestore-fe'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/fakestore-fe/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.use(requireHTTPS);
+app.use(express.static('./dist/fakestore'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/fakestore/'});
+  });
+  app.listen(process.env.PORT || 8080);
